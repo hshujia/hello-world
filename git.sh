8a1815ded1d0af16ad323ba4dfe9682cf43be771
hshujia <hshujia@qq.com>
2014-09-15 04:29:40 +0200

    add git.sh

---
+++ git.sh
@@ -0,0 +1,7 @@
+git init
+git add README.md
+#git config user.name hshujia
+#git config user.email hshujia@qq.com
+git commit -m "first commit"
+#git remote add origin git@hshujia.github.com:hshujia/hello-world.git
+git push -u origin master
